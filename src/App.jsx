9b0895269ec2d3827e2c8bd257262cacfbcdcc20
Onion <csr1000@naver.com>
2025-01-24 10:57:44 +0900

    라우터, redux, 편집기포맷 세팅

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import { Routes } from 'react-router-dom';
 import GlobalStyle from './styled/GlobalStyle';
+// import { ThemeProvider } from 'styled-components';
+// import { theme } from './styles/theme';
 
 const App = () => {
-  return <GlobalStyle />;
+  return (
+    <>
+      {/* <ThemeProvider theme={theme}> */}
+      <GlobalStyle />
+
+      <Routes>{/* 라우터 설정 */}</Routes>
+      {/* </ThemeProvider> */}
+    </>
+  );
 };
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,15 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
+import { store } from './app/store';
+import { Provider } from 'react-redux';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </StrictMode>
 );
