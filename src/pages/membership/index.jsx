2422f1ae33f44a9c954a835c95ec9349f12419d0
eunju98 <dmswn1321@gmail.com>
2025-02-12 11:04:37 +0900

    멤버쉽 수정화면 작업

--- src/pages/membership/index.jsx
+++ src/pages/membership/index.jsx
@@ -5,10 +5,12 @@ import {
   Ad,
   List,
   MembershipAd,
-  Pay,
-  ShortCut,
   MembershipBox,
   DivBox,
+  TextBox,
+  PayBox,
+  ShortcutBox,
+  ContInner,
 } from './style';
 
 const Membership = () => {
@@ -18,34 +20,46 @@ const Membership = () => {
         <MembershipAd>
           <img src="/public/images/pattern/membershipadd.png" alt="광고화면" />
         </MembershipAd>
-        <DivBox>
-          <List>
-            <li>
-              <DivWrap>
-                <Ad>스탠다드</Ad>
-                <Section>광고 없이 감상</Section>
-                <Section>프로필 1인 동시재생 1회선</Section>
-                <Section>최신화 시청</Section>
-                <Section>다운로드 지원</Section>
-                <Section>FHD 화질 지원</Section>
-                <Section>TV 앱 지원</Section>
-              </DivWrap>
-            </li>
-            <li>
-              <DivWrap>
-                <Ad>프리미엄</Ad>
-                <Section>광고 없이 감상</Section>
-                <Section>프로필 4인 동시재생 4회선</Section>
-                <Section>최신화 시청</Section>
-                <Section>다운로드 지원</Section>
-                <Section>FHD 화질 지원</Section>
-                <Section>TV 앱 지원</Section>
-              </DivWrap>
-            </li>
-          </List>
-          <Pay>이용권 구매하기</Pay>
-          <ShortCut>광고형 스텐다드 이용권이 궁금하시다면?</ShortCut>
-        </DivBox>
+
+        <ContInner>
+          <h2>이용권 구독</h2>
+          <TextBox>
+            <h1>나에게 맞는 멤버십을 확인하세요</h1>
+            <p>멤버십은 언제든 해지가 가능해요.</p>
+          </TextBox>
+          <DivBox>
+            <List>
+              <li>
+                <DivWrap>
+                  <Ad>스탠다드</Ad>
+                  <Section>광고 없이 감상</Section>
+                  <Section>프로필 1인 동시재생 1회선</Section>
+                  <Section>최신화 시청</Section>
+                  <Section>다운로드 지원</Section>
+                  <Section>FHD 화질 지원</Section>
+                  <Section>TV 앱 지원</Section>
+                </DivWrap>
+              </li>
+              <li>
+                <DivWrap>
+                  <Ad>프리미엄</Ad>
+                  <Section>광고 없이 감상</Section>
+                  <Section>프로필 4인 동시재생 4회선</Section>
+                  <Section>최신화 시청</Section>
+                  <Section>다운로드 지원</Section>
+                  <Section>FHD 화질 지원</Section>
+                  <Section>TV 앱 지원</Section>
+                </DivWrap>
+              </li>
+            </List>
+            <PayBox>
+              <button>이용권 구매하기</button>
+            </PayBox>
+            <ShortcutBox>
+              <button>광고형 스텐다드 이용권이 궁금하시다면?</button>
+            </ShortcutBox>
+          </DivBox>
+        </ContInner>
       </MembershipBox>
     </MembershipWrap>
   );
--- src/pages/membership/style.js
+++ src/pages/membership/style.js
@@ -5,11 +5,18 @@ export const MembershipWrap = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 3530px;
+  height: 1972px;
 `;
 
 export const MembershipBox = styled.div`
-  height: 3530px;
+  height: 1972px;
+
+  h2 {
+    font-size: 40px;
+    font-weight: bold;
+    font-family: 'Pretendard';
+    color: #ffffff;
+  }
 `;
 
 export const DivWrap = styled.div`
@@ -20,6 +27,7 @@ export const DivWrap = styled.div`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
+  padding-left: 55px;
 `;
 
 export const Section = styled.div`
@@ -53,43 +61,84 @@ export const List = styled.ul`
   gap: 60px;
 `;
 
+export const ContInner = styled.div`
+  padding: 0 123px;
+
+  h2 {
+    margin-bottom: 65px;
+  }
+`;
+
 export const MembershipAd = styled.div`
   width: 1927px;
   height: 482px;
   background-color: #4d94ff;
-  margin-bottom: 364px;
+  margin-bottom: 98px;
+`;
+
+export const TextBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 94px;
+
+  h1 {
+    font-size: 40px;
+    font-weight: bold;
+    font-family: 'Pretendard';
+    color: #ffffff;
+  }
+  p {
+    font-size: 25px;
+    font-weight: bold;
+    font-family: 'Pretendard';
+    color: #ffffff;
+  }
 `;
 
 export const DivBox = styled.div`
   justify-content: center;
 `;
 
-export const Pay = styled.div`
-  width: 449px;
-  height: 82px;
-  border-radius: 55px;
-  background-color: #0066ff;
-  margin-bottom: 364px;
+export const PayBox = styled.div`
   display: flex;
   justify-content: center;
-  color: #ffffff;
-  font-family: 'Pretendard';
-  align-items: center;
-  font-size: 30px;
-  font-weight: bold;
-  font-weight: 700;
-  line-height: 36px;
+
+  button {
+    width: 449px;
+    height: 82px;
+    border-radius: 55px;
+    background-color: #0066ff;
+    margin-bottom: 86px;
+    display: flex;
+    justify-content: center;
+    color: #ffffff;
+    font-family: 'Pretendard';
+    align-items: center;
+    font-size: 30px;
+    font-weight: bold;
+    font-weight: 700;
+    line-height: 36px;
+
+    margin-top: 94px;
+  }
 `;
-export const ShortCut = styled.div`
-  width: 1683px;
-  height: 92px;
-  background-color: #4d94ff;
-  border-radius: 18px;
-  color: #ffffff;
-  font-family: 'Pretendard';
-  align-items: center;
-  font-size: 30px;
-  font-weight: bold;
+
+export const ShortcutBox = styled.div`
   display: flex;
   justify-content: center;
+
+  button {
+    width: 1683px;
+    height: 92px;
+    background-color: #4d94ff;
+    border-radius: 18px;
+    color: #ffffff;
+    font-family: 'Pretendard';
+    align-items: center;
+    font-size: 30px;
+    font-weight: bold;
+    display: flex;
+    justify-content: center;
+  }
 `;
